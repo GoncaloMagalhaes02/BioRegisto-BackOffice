4feda29713beeff7ad9713c4655c2a32a5be38d1
GoncaloMagalhaes02 <goncamag02@gmail.com>
2026-05-28 15:06:43 +0100

    dependecies installed and initial routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import Login from "./page/Login/Login";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Login />}></Route>),
+);
+
+const router = createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
-)
+);
