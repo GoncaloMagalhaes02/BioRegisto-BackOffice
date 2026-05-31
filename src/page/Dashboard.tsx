6666137370c935ea08a6215f825779327b5bcd72
GoncaloMagalhaes02 <goncamag02@gmail.com>
2026-05-31 12:10:25 +0100

    table working

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -1,18 +1,61 @@
 import React, { useEffect, useState } from "react";
 
 import { useAuth } from "@/hooks/useAuth";
+import Badges from "@/components/Dashboard/badges";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { type Observation } from "@/types";
 import { supabase } from "@/lib/supabaseClient";
 
-import { Eye, CircleCheck, Clock, CircleX } from "lucide-react";
-import { useObservationStats } from "@/hooks/useObservationsStats";
-
 function Dashboard() {
-  const { profile } = useAuth();
+  const { profile, user, loading: authLoading } = useAuth();
 
-  const { observacoes, pendentes, rejeitadas, validadas } =
-    useObservationStats();
+  const [observations, setObservations] = useState<Observation[]>([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (authLoading || !user) return;
+
+    const getObservations = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("observations")
+          .select(
+            `
+    *,
+    profiles!observations_user_id_fkey ( username, full_name, avatar_url )
+  `,
+          )
+          .eq("status", "PENDING");
+
+        if (error) throw error;
+        console.log(data);
+        setObservations(data || []);
+      } catch (error) {
+        console.log("Erro:", error);
+      }
+    };
+
+    getObservations();
+  }, [user, authLoading]);
+
+  function formatDate(date: string | Date) {
+    return new Intl.DateTimeFormat("pt-PT", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    }).format(new Date(date));
+  }
 
   const data = new Date();
   return (
@@ -24,47 +67,56 @@ function Dashboard() {
           <p className="text-xs font-light">{data.toLocaleDateString()}</p>
         </div>
       </header>
+      {/* Badges da dashboard */}
+      <Badges />
 
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        <div className="flex flex-col p-5 bg-white relative rounded-lg">
-          <h3 className="text-gray-400">Total de observações</h3>
-          <p className="font-semibold text-4xl pb-5 pt-1">{observacoes}</p>
-          <p className="text-gray-600">+ 12% vs mês anterior</p>
-          <div className="absolute right-8 top-9 bg-gray-200 p-2 rounded-sm">
-            <Eye />
-          </div>
+      <section className="mt-9 bg-white">
+        <div className="flex w-full items-center justify-between py-6 px-5">
+          <h3 className="font-semibold">
+            Observações pendentes para validação
+          </h3>
+          <span className="text-orange-400 border border-orange-200 font-medium bg-orange-100 px-4 py-2 rounded-lg">
+            6 pendentes
+          </span>
         </div>
-        <div className="flex flex-col p-5 bg-white relative rounded-lg">
-          <h3 className="text-gray-400">Validadas</h3>
-          <p className="font-semibold text-green-500 text-4xl pb-5 pt-1">
-            {validadas}
-          </p>
-          <p className="text-green-500">+ 9% vs mês anterior</p>
-          <div className="absolute right-8 top-9 bg-green-100 p-2 rounded-sm">
-            <CircleCheck color="#46e30d" />
-          </div>
-        </div>
-        <div className="flex flex-col p-5 bg-white relative rounded-lg">
-          <h3 className="text-gray-400">Pendentes</h3>
-          <p className="font-semibold text-orange-400 text-4xl pb-5 pt-1">
-            {pendentes}
-          </p>
-          <p className="text-orange-500">+ 3% vs mês anterior</p>
-          <div className="absolute right-8 top-9 bg-orange-100 p-2 rounded-sm">
-            <Clock color="#f88a0d" />
-          </div>
-        </div>
-        <div className="flex flex-col p-5 bg-white relative rounded-lg">
-          <h3 className="text-gray-400">Rejeitadas</h3>
-          <p className="font-semibold text-red-600 text-4xl pb-5 pt-1">
-            {rejeitadas}
-          </p>
-          <p className="text-red-500">- 2% vs mês anterior</p>
-          <div className="absolute right-8 top-9 bg-red-100 p-2 rounded-sm">
-            <CircleX color="#fe0101" />
-          </div>
-        </div>
-      </div>
+        <Table>
+          <TableHeader className="bg-stone-100">
+            <TableRow>
+              <TableHead>Foto</TableHead>
+              <TableHead>Utilizador</TableHead>
+              <TableHead>Descrição</TableHead>
+              <TableHead>Data</TableHead>
+              <TableHead>Localização</TableHead>
+              <TableHead className="text-right">Ações</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {observations.length > 0 ? (
+              observations.map((obs) => (
+                <TableRow className="py-5" key={obs.id}>
+                  <TableCell>Foto</TableCell>
+                  <TableCell>{obs.profiles?.username}</TableCell>
+                  <TableCell>{obs.description}</TableCell>
+                  <TableCell>{formatDate(obs.observed_at)}</TableCell>
+                  <TableCell>
+                    {obs.location.latitude + " | " + obs.location.longitude}
+                  </TableCell>
+                  <TableCell className="flex items-center justify-end gap-2">
+                    <a className="bg-green-700 px-3 py-2 rounded-lg text-white font-semibold">
+                      Validar
+                    </a>
+                    <a className="text-orange-400 border border-orange-200 font-medium bg-orange-100 px-3 py-2 rounded-lg">
+                      Rejeitar
+                    </a>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <p>nada para listar</p>
+            )}
+          </TableBody>
+        </Table>
+      </section>
     </>
   );
 }
